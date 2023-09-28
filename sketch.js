f4171fdfce3942ef7aa966af040b03bf714f9e54
Matheus-Barbosa-Pizzi <129908053+Matheus-Barbosa-Pizzi@users.noreply.github.com>
2023-09-28 20:01:33 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2;
+var cars = [];
+var fuelImg, coinImg, coneImg, pneuImg;
+var fuels, coins, obstacles;
+
+function preload() {
+  backgroundImage = loadImage("./assets/planodefundo.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/pista.png");
+  fuelImg = loadImage("../assets/fuel.png");  
+  coinImg = loadImage("../assets/coin.png");
+  coneImg = loadImage("../assets/cone.png");
+  pneuImg = loadImage("../assets/pneu.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
